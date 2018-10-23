40218991fb4ad254ab40d460bafea721e2377e44
xuyanwei <xuyanwei@sinochem.com>
2018-10-23 14:25:10 +0800

    fix address&file

--- coindemo/contracts/voooooote/index.js
+++ coindemo/contracts/voooooote/index.js
@@ -7,23 +7,32 @@ var Web3 = require("web3");
 app.use(express.static('.'));
 
 let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-//这里导入你自己的ABI
-let abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
+let fs = require('fs');
+let code = fs.readFileSync('Voting.sol').toString();
+let solc = require('solc');
+
+let compiledCode = solc.compile(code);
+
+let abi = JSON.parse(compiledCode.contracts[':Voting'].interface);
+
+//let abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 let VotingContract = web3.eth.contract(abi);
 //这里要替换成你自己的地址
-let contractInstance = VotingContract.at('0xcadcaf0cf38ad0adc259b8426b723bb31faa899d');
+let contractInstance = VotingContract.at('0xC9D4D622936BeaDc39Fbf38cc1c2E866875D226A');
 
 app.get("/totalVotesFor", function(req, res) {
 	var voteName = req.query.voteName;
-	var vote_num=contractInstance.totalVotesFor.call(voteName).toString();
+	var vote_num = contractInstance.totalVotesFor.call(voteName).toString();
 	console.log(vote_num);
+	console.log(req);
 	res.send(vote_num);
 });
 
 app.get("/voteForCandidate", function(req, res) {
 	var voteName = req.query.voteName;
 	contractInstance.voteForCandidate(voteName, {from: web3.eth.accounts[0]});
-	var vote_num=contractInstance.totalVotesFor.call(voteName).toString();
+	var vote_num = contractInstance.totalVotesFor.call(voteName).toString();
 	res.send(vote_num);
 });
 
